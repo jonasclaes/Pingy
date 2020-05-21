cc9666baab6a7ad9e3ae03393a23d2620abe4753
Jonas Claes <jonasclaes2001@outlook.com>
2020-05-21 10:19:43 +0200

    V1.1-SNAPSHOT; Added turning on features.

--- src/main/java/be/jonasclaes/pingy/Pingy.java
+++ src/main/java/be/jonasclaes/pingy/Pingy.java
@@ -24,19 +24,29 @@ public class Pingy {
         System.out.println("Pingy has been started.");
 
         // Create new ICMP ping task.
-        ICMPTask icmpTask;
-        if (System.getProperty("ipv4addr") != null) {
-            icmpTask = new ICMPTask(System.getProperty("ipv4addr"), 500);
-        } else {
-            icmpTask = new ICMPTask("8.8.8.8", 500);
+        boolean testICMP = Boolean.getBoolean("testICMP");
+        if (testICMP) {
+            System.out.println("Doing ICMPv4 test.");
+
+            ICMPTask icmpTask;
+            if (System.getProperty("ipv4addr") != null) {
+                icmpTask = new ICMPTask(System.getProperty("ipv4addr"), 500);
+            } else {
+                icmpTask = new ICMPTask("8.8.8.8", 500);
+            }
         }
 
         // Create new ICMP6 ping task.
-        ICMP6Task icmp6Task;
-        if (System.getProperty("ipv6addr") != null) {
-            icmp6Task = new ICMP6Task(System.getProperty("ipv6addr"), 500);
-        } else {
-            icmp6Task = new ICMP6Task("2001:4860:4860::8888", 500);
+        boolean testICMP6 = Boolean.getBoolean("testICMP6");
+        if (testICMP6) {
+            System.out.println("Doing ICMPv6 test.");
+
+            ICMP6Task icmp6Task;
+            if (System.getProperty("ipv6addr") != null) {
+                icmp6Task = new ICMP6Task(System.getProperty("ipv6addr"), 500);
+            } else {
+                icmp6Task = new ICMP6Task("2001:4860:4860::8888", 500);
+            }
         }
     }
 }
